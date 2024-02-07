1774485825c2a6979fc799c9bfb64e24d52f27f6
Luiz Oliveira <carlos.oliveira@ipdec.org>
2024-02-07 13:37:18 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      synchronize: false,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      migrationsTableName: 'migrations',
+      migrations: ['dist/**/migrations/*{.ts,.js}'],
+      migrationsRun: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
